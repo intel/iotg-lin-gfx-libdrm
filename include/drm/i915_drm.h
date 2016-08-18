a35fbb80d159266859908d4f7ed3b078a047a4b8
Michel Thierry <michel.thierry@intel.com>
2016-08-18 13:58:43 -0700

    include/drm: Add return arguments for aperture2 ioctl
    DRM_I915_GEM_GET_APERTURE2 allows users to query about map_total_size,
    map_available_size & map_largest_size.
    
    At the moment this is an internal requirement; unless there is an
    opensource user it cannot be merged upstream, hence marked as
    MUST_REBASE.
    
    v2: Ported to drm-bxt, note the new value for
    I915_PARAM_HAS_GET_APERTURE2, 0x808 instead of 0x804.
    
    Change-Id: Iec4d3576cec2d1a156de406df69155659674c6de
    Orig-Change-Id: I64f12949c2c4aed04419317382c0bbc08532c3d9
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-33229
    Signed-off-by: Michel Thierry <michel.thierry@intel.com>
    Signed-off-by: kimsehun <se.hun.kim@intel.com>
    Reviewed-on: https://android.intel.com:443/485049
    Reviewed-on: https://android.intel.com:443/511779
    Signed-off-by: Jeff McGee <jeff.mcgee@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- include/drm/i915_drm.h
+++ include/drm/i915_drm.h
@@ -341,6 +341,10 @@ struct i915_ext_ioctl_data
 		struct i915_ext_ioctl_data)
 
 /* Extended ioctl definitions */
+#define DRM_I915_GEM_GET_APERTURE2     0x1
+
+#define DRM_IOCTL_I915_GEM_GET_APERTURE2 \
+	DRM_IOR(DRM_I915_GEM_GET_APERTURE2, struct drm_i915_gem_get_aperture2)
 
 /* Allow drivers to submit batchbuffers directly to hardware, relying
  * on the security mechanisms provided by hardware.
@@ -440,6 +444,7 @@ typedef struct drm_i915_irq_wait {
 
 /* Private (not upstreamed) parameters start from 0x800   */
 /* This helps to avoid conflicts with new upstream values */
+#define I915_PARAM_HAS_GET_APERTURE2     0x802
 
 typedef struct drm_i915_getparam {
 	__s32 param;
@@ -1115,6 +1120,32 @@ struct drm_i915_gem_get_aperture {
 	 * bytes
 	 */
 	__u64 aper_available_size;
+};
+
+struct drm_i915_gem_get_aperture2 {
+	/** Total size of the aperture used by i915_gem_execbuffer, in bytes */
+	__u64 aper_size;
+
+	/**
+	 * Available space in the aperture used by i915_gem_execbuffer, in
+	 * bytes
+	 */
+	__u64 aper_available_size;
+
+	/**
+	 * Total space in the mappable region of the aperture, in bytes
+	 */
+	__u64 map_total_size;
+
+	/**
+	 * Available space in the mappable region of the aperture, in bytes
+	 */
+	__u64 map_available_size;
+
+	/**
+	 * Single largest available region inside the mappable region, in bytes.
+	 */
+	__u64 map_largest_size;
 };
 
 struct drm_i915_get_pipe_from_crtc_id {
